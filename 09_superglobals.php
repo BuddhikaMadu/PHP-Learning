39a1be68f89e602257b3146c76ea77e45166a47c
Buddhika Madusanka <89043684+BuddhikaMadu@users.noreply.github.com>
2023-03-18 14:51:22 +0530

    Create 09_superglobals.php
    Learning materials about superglobals

---
+++ 09_superglobals.php
@@ -0,0 +1,26 @@
+<?PHP
+
+// =========== Superglobals =============
+
+/*
+
+$_GET = contains information about variables passed through a URL or a form.
+
+$_POST = contains information about variables passed through a form.
+
+$_COOKIE = contains information about variables passed through a cookie.
+
+$_SESSION = Contains information about variables passed through a session.
+
+$_SERVER = contains information about the server enviorenment.
+
+$_ENV = Contains information about the enviorenment variables.
+
+$_FILES = contains information about files uploaded to the script.
+
+$_REQUEST = contains infotmation about variables pass through the form or url.
+
+*/
+
+
+?>
